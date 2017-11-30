32ba2deeda019bbb5d07e2ceef91be342ca76f52
imaybeniki <nikkimay96@gmail.com>
2017-11-30 13:18:39 -0500

    First commit

---
+++ baker/Baker.cpp
@@ -0,0 +1,25 @@
+#include <mutex>
+#include <vector>
+#include "../includes/baker.h"
+using namespace std;
+
+Baker::Baker(int id):id(id)
+{
+}
+
+Baker::~Baker()
+{
+}
+
+void Baker::bake_and_box(ORDER &anOrder) {
+	
+}
+
+void Baker::beBaker() {
+	
+	while (!order_in_Q.empty()){
+
+
+	}
+
+}
