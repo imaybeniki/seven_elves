0b6ef3b3c7435249a861fd9aefb37f562fdb174f
dkennedy778 <derek.kennedy.13@cnu.edu>
2017-11-30 13:26:53 -0500

    Added functions to waiter

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -2,9 +2,14 @@
 #include "stdlib.h"
 
 #include "../includes/waiter.h"
+#include "../includes/file_IO.h"
+#include "../includes/externs.h"
+#include <queue>
 
 using namespace std;
-
+//ID is just a number used to identify this particular baker
+//(used with PRINT statements)
+//filename is what waiter reads in orders from
 Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){
 }
 
@@ -12,11 +17,32 @@ Waiter::~Waiter()
 {
 }
 
-//gets next Order(s) from file_IO
+//gets next Order from file_IO
+//if return == SUCCESS then anOrder
+//contains new order
+//otherwise return contains error
 int Waiter::getNext(ORDER &anOrder){
-	return SUCCESS;
+	int success = myIO.getNext(anOrder);
+	return success;
+}
+//contains a loop that will get orders from filename one at a time
+	//then puts them in order_in_Q then signals baker(s) using cv_order_inQ
+	//so they can be consumed by baker(s)
+	//when finished exits loop and signals baker(s) using cv_order_inQ that
+	//it is done using b_WaiterIsFinished
+void Waiter::beWaiter() {
+	ORDER tempOrder;
+	int success = getNext(tempOrder);
+	unique_lock<mutex> lck(mutex_order_inQ);
+	while(success == SUCCESS){
+		lck.lock();
+		order_in_Q.push(tempOrder); //need a lock around this
+		lck.unlock();
+		cv_order_inQ.notify_all(); //there is no way this is right
+		int success = getNext(tempOrder);
+	}
+	unique_lock<mutex> lck(m);
+	b_WaiterIsFinished = true;
 }
 
-void Waiter::beWaiter() {
-}
 
